1465196a554a0c28c63fb601ae08b9e0b723a55f
Marcel Rubin <marcelgabber89@gmail.com>
2023-11-12 20:35:32 +0100

    config vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    autoprefixer(),
+    // Otros plugins aquí...
+  ],
   server: {
     proxy: {
       '/socket.io': {
         target: 'ws://localhost:3000',
         ws: true,
       },
-    }
-  }
-})
+    },
+  },
+});
